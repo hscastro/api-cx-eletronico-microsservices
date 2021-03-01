04ae8e00d5b1bd9afe11b2e8e2e59d825403547b
hscastro <halisonsc5@gmail.com>
2021-03-01 16:55:57 -0300

    first commit

---
+++ cx-elet-eureka-server/src/main/java/com/hscastro/eureka/RhEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.hscastro.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class RhEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhEurekaServerApplication.class, args);
+	}
+
+}
---
+++ cx-ms-eletronico/src/main/java/com/hscastro/services/PaymentService.java
@@ -0,0 +1,26 @@
+package com.hscastro.services;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.hscastro.entities.Payment;
+import com.hscastro.entities.Worker;
+import com.hscastro.feignClients.WorkerFeignClient;
+
+@Service
+public class PaymentService {
+	
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
+	
+	public Payment getPayment(long workerId, int days) {
+			
+		Worker worker = workerFeignClient.findById(workerId).getBody();		
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
+	}
+}
