6a5d4186f465668a9eb6d34e0b18fb1311ed41a4
hscastro <halisonsc5@gmail.com>
2021-03-02 23:11:02 -0300

    Changes in templates e HomeController

--- caixa-eletronico/src/main/java/com/hscastro/cxeletronico/controllers/HomeController.java
+++ caixa-eletronico/src/main/java/com/hscastro/cxeletronico/controllers/HomeController.java
@@ -1,14 +1,14 @@
 package com.hscastro.cxeletronico.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@Controller
 public class HomeController {
 		
 	@GetMapping("/")
-	public String home() {
-		System.out.println("ok, teste");
+	public String home() {		
 		return "home";
 	}
 	
