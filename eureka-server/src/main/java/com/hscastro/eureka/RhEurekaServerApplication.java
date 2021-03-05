09b0559ff845e0cd4b247b0d46d2eb3bdc1eef47
hscastro <halisonsc5@gmail.com>
2021-03-05 10:17:42 -0300

    Revert "Add profiles test, dev and prod in microsservice"
    This reverts commit 50dad142b79f6356505b9a7318198954224a38b8.
    M-U

--- eureka-server/src/main/java/com/hscastro/eureka/RhEurekaServerApplication.java
+++ eureka-server/src/main/java/com/hscastro/eureka/RhEurekaServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaServerApplication {
+public class RhEurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
+		SpringApplication.run(RhEurekaServerApplication.class, args);
 	}
 
 }
