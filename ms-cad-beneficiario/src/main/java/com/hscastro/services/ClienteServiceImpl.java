7347722b80519816560dce563f09f5317ac19e6f
hscastro <halisonsc5@gmail.com>
2021-03-05 14:08:15 -0300

    Add postgres

--- ms-cad-beneficiario/src/main/java/com/hscastro/services/ClienteServiceImpl.java
+++ ms-cad-beneficiario/src/main/java/com/hscastro/services/ClienteServiceImpl.java
@@ -7,17 +7,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.hscastro.entities.Cliente;
+import com.hscastro.messages.ClienteSendMessage;
 import com.hscastro.repositories.ClienteRepository;
 
 @Service
 public class ClienteServiceImpl implements ClienteService {
 	
 	
-	@Autowired
 	private ClienteRepository repository;
+	private ClienteSendMessage clienteSendMessage;
+		
+	@Autowired
+	public ClienteServiceImpl(ClienteRepository repository, ClienteSendMessage clienteSendMessage) {
+		this.repository = repository;
+		this.clienteSendMessage = clienteSendMessage;
+	}
 
 	@Override
 	public Cliente save(Cliente cliente) {
+		//insere o cliente na fila do RabbitMQ
+		this.clienteSendMessage.sendMessage(cliente);
+		//salva e rotorna o cliente
 		return repository.save(cliente);
 	}
 
